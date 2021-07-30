db4961b66d57b98d4665bad9605c6939030d1471
MijMa <36820079+MijMa@users.noreply.github.com>
2021-07-30 11:41:20 +0300

    Checkbox functionality added

--- src/components/FormUtils.tsx
+++ src/components/FormUtils.tsx
@@ -1,27 +1,23 @@
-import React from 'react';
-import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import {
-  Box, 
-  Typography, 
-  Grid, 
-  TextField, 
-  FormControlLabel,
-  Checkbox, 
-  FormControl, 
-  Switch,
+  Box,
   Button,
-  FormHelperText,
-  FormGroup,
-  FormLabel,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  Switch,
+  TextField,
 } from '@material-ui/core';
-import { useStyles } from '../styles';
 import { Send } from '@material-ui/icons';
+import React from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { useStyles } from '../styles';
 
 const defaultValues = {
   heliumMix: false,
   aknowledged: false,
   bearings: false,
-  airshipStatus: "",
+  airshipStatus: '',
   coordinateW: "N: 40° 41' 21.2892''",
   coordinateS: "W: 74° 2' 40.2072''",
 };
@@ -36,100 +32,113 @@ type FormValues = {
 };
 
 export default function FormUtils() {
-
-  const { register, handleSubmit, control, formState: {errors} } = useForm<FormValues>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>();
   const classes = useStyles();
 
   const [state, setState] = React.useState({
-    checkedA: true,
+    aknowledged: true,
   });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
 
-  const onSubmit: SubmitHandler<FormValues> = data => {
-    console.log("do stuff on submit, oh and here's data too: " + data)
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log("do stuff on submit, oh and here's data too: " + data);
     //Data on JSON-muodossa mutta miksi switcheistä ei tule dataa ja miksi defaulttidataa ei oteta?
-    console.log("do stuff on submit, oh and here's data too: " + JSON.stringify(data))
-  }
-  
-    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl margin="normal">
-          <Grid component="label" container alignItems="center" spacing={1}>
-            <Box bgcolor="secondary.main" color="secondary.contrastText" p={1}>
-              Helium mix:
-            </Box>
-            <Grid item className={classes.formItemSpacing}>Inoptimal</Grid>
-            <Grid item>
-              <Controller
-                name="heliumMix"
-                control={control}
-                render={({ field }) => <Switch {...field} />}
-                defaultValue={false}
-              />
-            </Grid>
-            <Grid item>Optimal</Grid>
-          </Grid>
-          <FormControlLabel
-            control={
-              <Checkbox
-              /*registering is enough to get the empty string but it's not what we want */
-              {...register("aknowledged")}
-              defaultValue="false"
-              />
-            }
-            label="uncontrolled:"
-            labelPlacement="start"
-          />
+    console.log(
+      "do stuff on submit, oh and here's data too: " + JSON.stringify(data)
+    );
+  };
 
-          <FormControlLabel
-            {...register("bearings")}
-            control={<Checkbox color="primary" />}
-            label="Bearings set:"
-            labelPlacement="start"
-          />
-          
-          <TextField 
-            {...register("airshipStatus")}
-            label="Airship status" 
-            helperText="Ready?" 
-            variant="outlined"
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl margin="normal">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Box bgcolor="secondary.main" color="secondary.contrastText" p={1}>
+            Helium mix:
+          </Box>
+          <Grid item className={classes.formItemSpacing}>
+            Inoptimal
+          </Grid>
+          <Grid item>
+            <Controller
+              name="heliumMix"
+              control={control}
+              render={({ field }) => <Switch {...field} />}
+            />
+          </Grid>
+          <Grid item>Optimal</Grid>
+        </Grid>
+        <Controller
+          control={control}
+          name="aknowledged"
+          render={({ field }) => (
+            <FormControlLabel
+              control={<Checkbox {...field} />}
+              label="Aknowledged:"
+              labelPlacement="start"
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="bearings"
+          render={({ field }) => (
+            <FormControlLabel
+              control={<Checkbox {...field} />}
+              label="Bearings set:"
+              labelPlacement="start"
+            />
+          )}
+        />
+
+        <TextField
+          {...register('airshipStatus')}
+          label="Airship status"
+          helperText="Ready?"
+          variant="outlined"
+          margin="normal"
+        />
+        {errors.airshipStatus && <span>This field is required</span>}
+
+        <Grid component="label" container alignItems="center" spacing={0}>
+          <TextField
+            {...register('coordinateW')}
+            disabled
+            id="filled-disabled"
+            label="Latitude"
+            defaultValue="N: 40° 41' 21.2892''"
+            variant="filled"
             margin="normal"
           />
-          {errors.airshipStatus && <span>This field is required</span>}
-
-          <Grid component="label" container alignItems="center" spacing={0}>
-            <TextField
-              {...register("coordinateW")}
-              disabled
-              id="filled-disabled"
-              label="Latitude"
-              defaultValue="N: 40° 41' 21.2892''"
-              variant="filled"
-              margin="normal"
-            />
-            <TextField className={classes.formItemSpacing}
-              {...register("coordinateS")}
-              disabled
-              id="filled-disabled"
-              label="Longitude"
-              defaultValue="W: 74° 2' 40.2072''"
-              variant="filled"
-              margin="normal"
-            />
-          </Grid>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            endIcon={<Send />}
-          >
-            Send
-          </Button>
-        </FormControl>
-      </form>
+          <TextField
+            className={classes.formItemSpacing}
+            {...register('coordinateS')}
+            disabled
+            id="filled-disabled"
+            label="Longitude"
+            defaultValue="W: 74° 2' 40.2072''"
+            variant="filled"
+            margin="normal"
+          />
+        </Grid>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          endIcon={<Send />}
+        >
+          Send
+        </Button>
+      </FormControl>
+    </form>
   );
 }
